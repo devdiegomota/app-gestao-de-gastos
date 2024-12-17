0e6fa5228c0036e16f50dc86c45e9edc4ab75013
Diego Mota <diego.motasocial@gmail.com>
2024-12-16 22:16:35 -0300

    a

--- validation.js
+++ validation.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 //função para validar conteudo do email caso receba valor
 function ValidarEmail(email) {
     var emailPattern =  /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
      return emailPattern.test(email); 
-=======
-//função para validar conteudo do email caso receba valor
-function ValidarEmail(email) {
-    var emailPattern =  /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-     return emailPattern.test(email); 
->>>>>>> 5705f5084c6d10ffe4789fcb08da13ba4700951d
 }
